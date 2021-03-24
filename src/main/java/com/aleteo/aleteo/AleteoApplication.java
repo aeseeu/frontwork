7b7aa736c7e18133daf3de6bedc105bd1a86abfa
aeseeu <psk6269@naver.com>
2021-03-25 01:13:23 +0900

    3.25

---
+++ src/main/java/com/aleteo/aleteo/AleteoApplication.java
@@ -0,0 +1,13 @@
+package com.aleteo.aleteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleteoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AleteoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aleteo/aleteo/AleteoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleteo.aleteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleteoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
